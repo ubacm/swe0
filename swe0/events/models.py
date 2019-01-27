15f267980d22b6ab523b75a7d8f3d5ee5d39afa9
Stephen James <stephenorjames@gmail.com>
2019-01-27 15:39:12 -0500

    Create basic Event model

--- swe0/events/models.py
+++ swe0/events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=50, blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        ordering = ('start_time', 'end_time', 'title')
+
+    def __str__(self):
+        return self.title
