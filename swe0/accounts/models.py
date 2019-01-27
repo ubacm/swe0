370728ee4d02814b077eb6f7da2386e60c76b60b
Stephen James <stephenorjames@gmail.com>
2019-01-27 00:05:07 -0500

    Create custom user model and manager

--- swe0/accounts/models.py
+++ swe0/accounts/models.py
@@ -1,3 +1,61 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **kwargs):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **kwargs):
+        kwargs.setdefault('is_staff', False)
+        kwargs.setdefault('is_superuser', False)
+        return self._create_user(email, password, **kwargs)
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+
+        if kwargs.get('is_staff') is not True:
+            raise ValueError('Superusers must have is_staff=True')
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError('Superusers must have is_superuser=True')
+
+        return self._create_user(email, password, **kwargs)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('email address', max_length=255, unique=True)
+    name = models.CharField(max_length=100)  # Note: Slack's limit is 80 chars.
+
+    is_staff = models.BooleanField(
+        default=False,
+        help_text='Designates whether the user can log into the admin site.',
+    )
+    is_active = models.BooleanField(
+        default=True,
+        help_text='Designates whether this user account is enabled/usable.',
+    )
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('name',)
+
+    class Meta:
+        swappable = 'AUTH_USER_MODEL'
+        ordering = ('name', 'email')
+
+    def __str__(self):
+        return '{} <{}>'.format(self.name, self.email)
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
