0271a950edab0cfa465311a20a3ae78f948f6fdd
Stephen James <stephenorjames@gmail.com>
2019-01-27 00:21:06 -0500

    Register custom User model in admin

--- swe0/accounts/admin.py
+++ swe0/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from swe0.accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('name', 'email', 'password')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('name', 'email')
+    search_fields = ('name', 'email')
+    ordering = ('name', 'email')
