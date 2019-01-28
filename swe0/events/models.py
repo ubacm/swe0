35b90c6c44128910930e0fff4087f3187abc57fb
Stephen James <stephenorjames@gmail.com>
2019-01-28 15:07:00 -0500

    Update events models to add attendance tracking

--- swe0/events/models.py
+++ swe0/events/models.py
@@ -1,4 +1,8 @@
+import itertools
+
+from django.conf import settings
 from django.db import models
+from django.utils.crypto import get_random_string
 
 
 class Event(models.Model):
@@ -8,8 +12,42 @@ class Event(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField(blank=True, null=True)
 
+    attendance_enabled = models.BooleanField(default=False)
+    attendance_code = models.CharField(max_length=25, unique=True, blank=True)
+    attendance_rewards = models.IntegerField(default=1)
+
+    # Attempt to eliminate potentially confusing characters.
+    ATTENDANCE_CODE_ALLOWED_CHARS = 'ABDEFGHJMNPQRTYabdefghjmnpqrty23456789'
+
     class Meta:
         ordering = ('start_time', 'end_time', 'title')
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.attendance_code:
+            self.attendance_code = self.generate_attendance_code()
+        return super().save(*args, **kwargs)
+
+    @classmethod
+    def generate_attendance_code(cls):
+        """Generate a unique attendance code for an Event."""
+        for _ in itertools.count():
+            code = get_random_string(
+                length=6,
+                allowed_chars=cls.ATTENDANCE_CODE_ALLOWED_CHARS,
+            )
+            if not cls.objects.filter(attendance_code=code).exists():
+                return code
+
+
+class Attendance(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('event', 'user')
+
+    def __str__(self):
+        return '{} at {}'.format(self.user, self.event)
