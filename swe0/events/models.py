e43a0cb3350b078d0b2c26dac2a288b7e1389a47
Stephen James <stephenorjames@gmail.com>
2019-01-30 02:01:51 -0500

    Changes all "attendance" references to "check in"

--- swe0/events/models.py
+++ swe0/events/models.py
@@ -12,12 +12,12 @@ class Event(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField(blank=True, null=True)
 
-    attendance_enabled = models.BooleanField(default=False)
-    attendance_code = models.CharField(max_length=25, unique=True, blank=True)
-    attendance_rewards = models.IntegerField(default=1)
+    check_in_enabled = models.BooleanField(default=False)
+    check_in_code = models.CharField(max_length=25, unique=True, blank=True)
+    check_in_rewards = models.IntegerField(default=1)
 
     # Attempt to eliminate potentially confusing characters.
-    ATTENDANCE_CODE_ALLOWED_CHARS = 'ABDEFGHJMNPQRTYabdefghjmnpqrty23456789'
+    CHECK_IN_CODE_ALLOWED_CHARS = 'ABDEFGHJMNPQRTYabdefghjmnpqrty23456789'
 
     class Meta:
         ordering = ('start_time', 'end_time', 'title')
@@ -26,23 +26,23 @@ class Event(models.Model):
         return self.title
 
     def save(self, *args, **kwargs):
-        if not self.attendance_code:
-            self.attendance_code = self.generate_attendance_code()
+        if not self.check_in_code:
+            self.check_in_code = self.generate_check_in_code()
         return super().save(*args, **kwargs)
 
     @classmethod
-    def generate_attendance_code(cls):
-        """Generate a unique attendance code for an Event."""
+    def generate_check_in_code(cls):
+        """Generate a unique check in code for an Event."""
         for _ in itertools.count():
             code = get_random_string(
                 length=6,
-                allowed_chars=cls.ATTENDANCE_CODE_ALLOWED_CHARS,
+                allowed_chars=cls.CHECK_IN_CODE_ALLOWED_CHARS,
             )
             if not cls.objects.filter(attendance_code=code).exists():
                 return code
 
 
-class Attendance(models.Model):
+class CheckIn(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
