926709398e1ee488b20be081efe0cd2a23fdf49b
Stephen James <stephenorjames@gmail.com>
2019-01-30 01:50:05 -0500

    Implement view for uploading resume

--- swe0/profiles/views.py
+++ swe0/profiles/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+
+from swe0.profiles.models import Profile
+
+
+class ResumeUploadView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    template_name = 'profiles/resume_upload.html'
+    model = Profile
+    fields = ('resume',)
+    success_message = 'Your resume was successfully updated.'
+    # TODO: use a more appropriate view for success_url when available
+    success_url = reverse_lazy('profiles:resume-upload')
+
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.get_queryset()
+        obj, created = queryset.get_or_create(user=self.request.user)
+        return obj
+
+    def form_valid(self, form):
+        previous_resume = form['resume'].initial
+        response = super().form_valid(form)
+        if previous_resume:
+            os.remove(previous_resume.path)
+        return response
