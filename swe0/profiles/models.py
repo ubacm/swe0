02d1740304d594464bb85f0ca162c9a99ad71e2d
Stephen James <stephenorjames@gmail.com>
2019-01-29 23:26:43 -0500

    Create Profile model with resume field

--- swe0/profiles/models.py
+++ swe0/profiles/models.py
@@ -1,3 +1,27 @@
+import itertools
+import os
+import uuid
+
+from django.conf import settings
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
-# Create your models here.
+
+def _generate_resume_filename(*_args):
+    for _ in itertools.count():
+        filename = os.path.join(
+            'profiles',
+            'resumes',
+            '{}.pdf'.format(uuid.uuid4().hex),
+        )
+        if not Profile.objects.filter(resume=filename).exists():
+            return filename
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    resume = models.FileField(
+        upload_to=_generate_resume_filename,
+        validators=[FileExtensionValidator(['pdf'])],
+        blank=True,
+    )
