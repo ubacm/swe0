d8153d4ea53e26f65d223eb8c20b316b73ebf328
Stephen James <stephenorjames@gmail.com>
2019-01-30 01:22:37 -0500

    Configure directory & URL for static & media files

--- swe0/settings.py
+++ swe0/settings.py
@@ -151,6 +151,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # If provided, only users whose email address domain is included here
