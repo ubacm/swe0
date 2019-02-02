33f28af12507121cad48f42932853a68f4990cea
Stephen James <stephenorjames@gmail.com>
2019-02-02 01:10:55 -0500

    Create models for polls: Poll, Entry, Vote

--- swe0/polls/admin.py
+++ swe0/polls/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from swe0.polls import models
+
+
+admin.site.register(models.Entry)
+
+
+class PollEntriesInline(admin.TabularInline):
+    model = models.Poll.entries.through
+
+
+@admin.register(models.Poll)
+class PollAdmin(admin.ModelAdmin):
+    inlines = (PollEntriesInline,)
+    exclude = ('entries',)
+    list_display = ('name', 'event', 'is_accepting_entries', 'is_accepting_votes')
+
+
+@admin.register(models.Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('poll', 'user', 'entry')
--- swe0/polls/models.py
+++ swe0/polls/models.py
@@ -1,3 +1,49 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Entry(models.Model):
+    """An Entry is an entity that can be voted on in a poll."""
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return self.name
+
+
+class Poll(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    event = models.ForeignKey(
+        'events.Event',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    is_accepting_entries = models.BooleanField(
+        default=False,
+        help_text='Are users able to submit entries?',
+    )
+    is_accepting_votes = models.BooleanField(
+        default=False,
+        help_text='Are users able to vote on entries in this poll?',
+    )
+    entries = models.ManyToManyField(Entry, related_name='polls', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'poll', 'entry')
+
+    def __str__(self):
+        return '{} on {}'.format(self.user, self.poll)
