f66ec8b687ebd2ae6e4f221533a3f406849dd6ba
Stephen James <stephenorjames@gmail.com>
2019-02-04 01:06:14 -0500

    Add log in view as index for OAuth

--- swe0/accounts/urls.py
+++ swe0/accounts/urls.py
@@ -6,5 +6,6 @@ from swe0.accounts import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('login/', views.LogInView.as_view(), name='log-in'),
     path('logout/', views.LogOutView.as_view(), name='log-out'),
 ]
--- swe0/accounts/views.py
+++ swe0/accounts/views.py
@@ -1,4 +1,15 @@
 from django.contrib.auth.views import LogoutView as DjangoLogOutView
+from django.shortcuts import redirect, reverse
+from django.views.generic import TemplateView
+
+
+class LogInView(TemplateView):
+    template_name = 'accounts/log_in.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse('core:home'))
+        return super().dispatch(request, *args, **kwargs)
 
 
 class LogOutView(DjangoLogOutView):
