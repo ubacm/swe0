eaf1377ad5aa017014b5f2ba70f18cc72ba79e89
Stephen James <stephenorjames@gmail.com>
2019-02-09 20:42:14 -0500

    Allow reading MEDIA_ROOT from environment variable

--- swe0/settings.py
+++ swe0/settings.py
@@ -160,7 +160,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.getenv('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
 MEDIA_URL = '/media/'
 
 
