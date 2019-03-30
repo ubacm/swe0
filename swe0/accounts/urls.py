b0c1da6651afcbeda01d98258b1eec8415368175
Stephen James <stephenorjames@gmail.com>
2019-03-30 01:15:23 -0400

    Create separate page for local logins

--- swe0/accounts/urls.py
+++ swe0/accounts/urls.py
@@ -7,5 +7,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.LogInView.as_view(), name='log-in'),
+    path('login/local/', views.LocalLogInView.as_view(), name='local-log-in'),
     path('logout/', views.LogOutView.as_view(), name='log-out'),
 ]
--- swe0/accounts/views.py
+++ swe0/accounts/views.py
@@ -1,4 +1,8 @@
-from django.contrib.auth.views import LogoutView as DjangoLogOutView
+from django.conf import settings
+from django.contrib.auth.views import (
+    LoginView as DjangoLogInView,
+    LogoutView as DjangoLogOutView,
+)
 from django.shortcuts import redirect, reverse
 from django.views.generic import TemplateView
 
@@ -10,6 +14,13 @@ class LogInView(TemplateView):
         if request.user.is_authenticated:
             return redirect(reverse('core:home'))
         return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs, show_local_login_link=settings.DEBUG)
+
+
+class LocalLogInView(DjangoLogInView):
+    template_name = 'accounts/local_log_in.html'
 
 
 class LogOutView(DjangoLogOutView):
