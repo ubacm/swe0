50d7c55439bc49ba7c78c1a43191afdf433376fd
Stephen James <stephenorjames@gmail.com>
2019-03-31 02:26:46 -0400

    Create views for voting and creating entries

--- swe0/polls/urls.py
+++ swe0/polls/urls.py
@@ -6,7 +6,9 @@ from swe0.polls import views
 app_name = 'polls'
 
 urlpatterns = [
-    path('', views.PollList.as_view(), name='list'),
-    path('<int:pk>/', views.PollDetail.as_view(), name='detail'),
-    path('entries/<int:pk>/', views.EntryDetail.as_view(), name='entry-detail'),
+    path('', views.PollListView.as_view(), name='list'),
+    path('<int:pk>/', views.PollDetailView.as_view(), name='detail'),
+    path('<int:poll_pk>/vote/<int:entry_pk>/', views.VoteView.as_view(), name='vote'),
+    path('entries/create/', views.EntryCreateView.as_view(), name='entry-create'),
+    path('entries/<int:pk>/', views.EntryDetailView.as_view(), name='entry-detail'),
 ]
--- swe0/polls/views.py
+++ swe0/polls/views.py
@@ -1,16 +1,79 @@
-from django.views.generic import DetailView, ListView
+from django import forms
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.views.generic import CreateView, DetailView, FormView, ListView
 
-from swe0.polls.models import Entry, Poll
+from swe0.polls.models import Entry, Poll, Vote
 
 
-class EntryDetail(DetailView):
+class EntryCreateView(LoginRequiredMixin, CreateView):
+    model = Entry
+    fields = ('name', 'description')
+
+
+class EntryDetailView(DetailView):
     model = Entry
 
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(
+            **kwargs,
+            polls=self.object.polls.filter(is_accepting_votes=True),
+        )
 
-class PollDetail(DetailView):
+
+class PollDetailView(DetailView):
     model = Poll
 
 
-class PollList(ListView):
+class PollListView(ListView):
     model = Poll
     ordering = ['-id']
+
+
+class VoteView(LoginRequiredMixin, FormView):
+    template_name = 'polls/vote_form.html'
+    form_class = forms.Form
+
+    def dispatch(self, request, *args, **kwargs):
+        self.poll = get_object_or_404(Poll, pk=kwargs.get('poll_pk'))
+        self.entry = get_object_or_404(
+            Entry,
+            pk=kwargs.get('entry_pk'),
+            polls=self.poll,
+        )
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(
+            **kwargs,
+            poll=self.poll,
+            entry=self.entry,
+        )
+
+    def form_valid(self, form):
+        vote, created = Vote.objects.get_or_create(
+            user=self.request.user,
+            poll=self.poll,
+            entry=self.entry,
+        )
+        if created:
+            messages.success(
+                self.request,
+                'You have voted for {!r} in {!r}.'.format(
+                    self.entry,
+                    self.poll,
+                ),
+            )
+        else:
+            messages.info(
+                self.request,
+                'You already voted for {!r} in {!r}.'.format(
+                    self.entry,
+                    self.poll,
+                ),
+            )
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.entry.get_absolute_url()
